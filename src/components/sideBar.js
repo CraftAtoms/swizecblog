031aa5a76016eb175246653e9fd16459378bbe36
CraftAtoms <creator@craftatoms.com>
2018-07-22 18:43:34 -0400

    Sidbar Social

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
+import Link from "gatsby-link";
+
 import Me from "../images/me.png"
+import './sideBar.css'
 
 const Wrapper = styled.div`
 
@@ -14,7 +17,22 @@ color: grey;
 img {
     height: 100px
 }
-`;
+`
+
+const Icons = styled.div`
+color: #7c51a1;
+a {
+  margin: 0 .3rem;
+  text-decoration: none;
+  color: #7c51a1;
+}
+a:hover {
+  color: #000;
+}
+h4:hover {
+text-decoration: underline;
+}
+`
 
 const Sidebar = () => (
   
@@ -30,6 +48,24 @@ const Sidebar = () => (
         
         Over the years I've helped over 10,000 engineers hone their craft. Engineers have used my books, articles, and talks to improve their technical skills, get promotions, change jobs, and ship their products faster.
         </p>
+        
+        <Icons>
+        <Link to='/'>
+        <h4>More About Me</h4>
+        </Link>
+          <a href="mailto:hi@swizec.com?subject=Hi!" className="social" title="Email me">
+            <i class="ss-icon">email</i>
+          </a>
+          <a href="https://github.com/Swizec" className="social" title="See my github profile">
+            <i className="ss-icon">octocat</i>
+          </a>
+          <a href="https://twitter.com/Swizec" class="social" title="Follow me on twitter">
+            <i className="ss-icon">twitter</i>
+          </a>
+          <a href="http://swizec.com/blog/feed" class="social" title="Subscribe to RSS">
+            <i className="ss-icon">rss</i>
+          </a>
+        </Icons>
       </Wrapper>
 )
 
